e8e1f2f84397fc4a0e087c3ffdc419d28cdcf2dc
diSty <distty@gmail.com>
2023-08-16 04:04:50 +0300

    fixed unloading models and some more

--- layout/models_bar.py
+++ layout/models_bar.py
@@ -6,7 +6,8 @@ import os
 LORA_PATH ='lora_models_path'
 MODEL_PATH ='models_path'
 
-def set_models_path(extensions, models_path_directory=None, default_models_path='models/checkpoints', settings_key=MODEL_PATH):
+def set_models_path(extensions, models_path_directory=None, default_models_path='models/checkpoints', settings_key='models_path', recursive=False):
+
     file_list = [
         ""
     ]
@@ -15,25 +16,30 @@ def set_models_path(extensions, models_path_directory=None, default_models_path=
         sg.user_settings_set_entry(settings_key, default_models_path)
         models_path_directory = sg.user_settings_get_entry(settings_key, '')
 
-    idx = 0
     if models_path_directory and os.path.isdir(models_path_directory):
         try:
-            for root, dirs, files in os.walk(models_path_directory):
-                for filename in files:
+            if recursive:
+                for root, dirs, files in os.walk(models_path_directory):
+                    for filename in files:
+                        if isinstance(extensions, (list, tuple)) and filename.endswith(tuple(extensions)):
+                            full_path = os.path.join(root, filename)
+                            relative_path = os.path.relpath(full_path, models_path_directory)
+                            file_list.append(relative_path)
+            else:
+                for filename in os.listdir(models_path_directory):
                     if isinstance(extensions, (list, tuple)) and filename.endswith(tuple(extensions)):
-                        full_path = os.path.join(root, filename)
-                        relative_path = os.path.relpath(full_path, models_path_directory)
-                        file_list.append(relative_path)
-                        # # window.write_event_value('-progress_bar-', steps_idx)
-                        # print('file',idx)
+                        file_list.append(filename)
                     
         except PermissionError:
             sg.popup_error(f"Permission denied when accessing directory: {models_path_directory}")
 
     return file_list
 
-models_list = set_models_path(extensions, sg.user_settings_get_entry(MODEL_PATH, ''), settings_key=MODEL_PATH)
-lora_models_list = set_models_path(extensions, sg.user_settings_get_entry(LORA_PATH, ''), settings_key=LORA_PATH)
+use_sub_folders_model = sg.user_settings_get_entry("use_sub_folders_model")
+use_sub_folders_model_lora = sg.user_settings_get_entry("use_sub_folders_lora")
+
+models_list = set_models_path(extensions, sg.user_settings_get_entry(MODEL_PATH, ''), settings_key=MODEL_PATH, recursive=use_sub_folders_model)
+lora_models_list = set_models_path(extensions, sg.user_settings_get_entry(LORA_PATH, ''), settings_key=LORA_PATH, recursive=use_sub_folders_model_lora)
 
 def create_layout():
 
@@ -42,6 +48,8 @@ def create_layout():
                 sg.Frame('Model',[
                     [    
                         sg.Combo(sorted(models_list), default_value=sg.user_settings_get_entry("selected_model", ''), size=(80, 1), key="-selected_model-",expand_x=True,enable_events=True, readonly=True),  
+                        sg.Checkbox('Sub-Folders',k='-use_sub_folders_model-',default=use_sub_folders_model,enable_events=True),
+
                         sg.I(visible=False,key='-models_path-',enable_events=True),
                         sg.FolderBrowse('Set Folder',enable_events=True),
                         sg.Button('Reload',k='-reload_models_path-'),  
@@ -53,8 +61,8 @@ def create_layout():
             
                 sg.Frame('LoRa',[
                     [    
-                        # sg.Text('Model',size=(6,1)),
                         sg.Combo(sorted(lora_models_list), default_value=sg.user_settings_get_entry("selected_lora", ''), size=(80, 1), key="-selected_lora-",expand_x=True,enable_events=True, readonly=True),  
+                        sg.Checkbox('Sub-Folders',k='-use_sub_folders_lora-',default=use_sub_folders_model_lora,enable_events=True),
                         sg.I(visible=False,key='-lora_path-',enable_events=True),
                         sg.FolderBrowse('Set Folder',enable_events=True),  
                         sg.Button('Reload',k='-reload_lora_path-'),    
@@ -70,9 +78,15 @@ def create_layout():
 
 def events(event, values, window):    
 
+    if event == '-use_sub_folders_model-':
+        sg.user_settings_get_entry("use_sub_folders_model", sg.user_settings_set_entry("use_sub_folders_model", values['-use_sub_folders_model-']))
+
+    if event == '-use_sub_folders_lora-':
+        sg.user_settings_get_entry("use_sub_folders_lora", sg.user_settings_set_entry("use_sub_folders_lora", values['-use_sub_folders_lora-']))
+
     if event == '-models_path-':
         models_path_directory = sg.user_settings_get_entry(MODEL_PATH, sg.user_settings_set_entry(MODEL_PATH, values['-models_path-']))
-        models_list = set_models_path(extensions, models_path_directory, settings_key=MODEL_PATH)
+        models_list = set_models_path(extensions, models_path_directory, settings_key=MODEL_PATH, recursive=values['-use_sub_folders_model-'])
         if models_list:
             window['-selected_model-'].update(values=sorted(models_list))
                 
@@ -81,7 +95,7 @@ def events(event, values, window):
 
     if event == '-lora_path-':
         lora_models_path_directory = sg.user_settings_get_entry(LORA_PATH, sg.user_settings_set_entry(LORA_PATH, values['-lora_path-']))
-        lora_models_list = set_models_path(extensions, lora_models_path_directory, settings_key=LORA_PATH)
+        lora_models_list = set_models_path(extensions, lora_models_path_directory, settings_key=LORA_PATH, recursive=values['-use_sub_folders_lora-'])
         if lora_models_list:
             window['-selected_lora-'].update(values=sorted(lora_models_list))
                 
@@ -90,13 +104,13 @@ def events(event, values, window):
 
     if event == '-reload_models_path-':
         models_path_directory = sg.user_settings_get_entry(MODEL_PATH)
-        models_list = set_models_path(extensions, models_path_directory, settings_key=MODEL_PATH)
+        models_list = set_models_path(extensions, models_path_directory, settings_key=MODEL_PATH, recursive=values['-use_sub_folders_model-'])
         if models_list:
             window['-selected_model-'].update(values=sorted(models_list))
 
     if event == '-reload_lora_path-':
         lora_models_path_directory = sg.user_settings_get_entry(LORA_PATH)
-        lora_models_list = set_models_path(extensions, lora_models_path_directory, settings_key=LORA_PATH)
+        lora_models_list = set_models_path(extensions, lora_models_path_directory, settings_key=LORA_PATH, recursive=values['-use_sub_folders_lora-'])
         if lora_models_list:
             window['-selected_lora-'].update(values=sorted(lora_models_list))
 
